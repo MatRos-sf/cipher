0a2c0d8e307855aa4fff94d44124c145d6230d47
MatRos-sf <mateusz-rosenkranz@wp.pl>
2023-09-02 10:55:20 +0200

    [features] Clipher.run add options exit

--- menu/menu.py
+++ menu/menu.py
@@ -1,26 +1,45 @@
-class Executor:
-    pass
+from typing import Dict, Tuple,List
+from functools import partial
 
+
+class Executor:
+
+    def encrypt(self):
+        code = input("Write text to encrypt\n>")
+        return "BLabla"
+
+    def exit(self):
+        return
 
 class Menu:
 
     def __init__(self):
         self.executor = Executor()
+        #self.options: Dict[int, Tuple[str, Executor]]
         self.options = {
-            1: "Encryption",
-            2: "Decrypting",
-            3: "Exit"
+            1: ("Encryption", partial(self.executor.encrypt)),
+            2: ("Exit", partial(self.executor.exit))
         }
 
-    def show_menu(self):
+    def show(self):
 
-        menu = [f"{key}: {value}" for key, value in self.options.items()]
-        choice = input("\n".join(menu))
-        print(choice)
+        menu = [f"{key}: {value[0]}" for key, value in self.options.items()]
+        print("\n".join(menu))
 
+    def response_menu(self):
+        key: List[int] = [k for k in self.options.keys()]
+        try:
+            user_response = input('> ')
+        except ValueError:
+            print("This option is unavailable!")
+        else:
+            self.execute(user_response)
     def execute(self, choice: int) -> None:
-        #self.executor.get(choice, self.show_error)()
+        a = self.options.get(choice, self.show_error)
+        print(a[0])
+
+
 
     def show_error(self):
-        print()
+        print('Error')
 
