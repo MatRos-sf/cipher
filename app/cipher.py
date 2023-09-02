99a4aaeec496feb0e86ac15d3acbd569d68576cf
MatRos-sf <mateusz-rosenkranz@wp.pl>
2023-09-02 10:59:46 +0200

    [features] Add exit to Clipher.run

--- app/cipher.py
+++ app/cipher.py
@@ -1,8 +1,31 @@
+import time
+
+from menu.menu import Menu
+
 class Cipher:
     def __init__(self):
         self.menu = Menu()
         self._run = True
+        self.buffor = []
+
     def run(self):
 
         while self._run:
             self.menu.show()
+            response, task = self.response()
+            if response:
+                if not task:
+                    self._run = False
+
+
+
+    def response(self) -> tuple:
+        try:
+            user_response = int(input("> "))
+        except ValueError:
+            print("This option is unavailable!")
+        else:
+            return user_response, self.menu.options.get(user_response, "Error")[1]()
+
+        return ()
+
