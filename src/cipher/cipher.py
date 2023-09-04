6d99033d5cde106004ac5b2067dee5025056f3d3
MatRos-sf <mateusz-rosenkranz@wp.pl>
2023-09-05 00:30:25 +0200

    [features] Added typing and docstring to Cipher class.

--- src/cipher/cipher.py
+++ src/cipher/cipher.py
@@ -9,15 +9,22 @@ class Cipher:
         self._run = True
 
     def run(self) -> None:
+        """Main method who run whole app."""
         # self.menu.load_buffers()
         while self._run:
             self.menu.show()
-            response, task = self.response()
+            response, result = self.response()
             if response:
-                if not task and self.menu.is_exit(response):
+                if not result and self.menu.is_exit(response):
                     self._run = False
 
     def response(self) -> Union[Tuple[None, None], Tuple[int, None], Tuple[int, dict]]:
+        """The method that waits for user input and check its.There are three ways to return this function :
+            [None, None] If the user selects the wrong type in the menu
+            [int, None] If the user selects correct type but something was wrong e.x. this option doesn't exist
+            [int, dict] If everything is ok
+        """
+
         try:
             user_response = int(input("> "))
         except ValueError:
