fbfdc3bc83cc451f1705b751462f8c8b9edcbf9e
MatRos-sf <mateusz-rosenkranz@wp.pl>
2023-09-05 12:54:51 +0200

    [features] Edited simplify chained comparsion in Ceasar.

--- src/ceaser/ceaser.py
+++ src/ceaser/ceaser.py
@@ -8,9 +8,6 @@ LEN_ALPHA = len(ALPHA_UPPER)
 
 
 class CaesarCipher:
-    alpha_upper = list(string.ascii_uppercase)
-    size_alpha = len(alpha_upper)
-
     def __init__(self, rot_types: List[int]):
         self._rot_types = rot_types
 
@@ -24,11 +21,11 @@ class CaesarCipher:
         index = ALPHA_UPPER.index(letter.upper()) if not is_digit else DIGITS.index(letter)
         new_index = index + num_rot
 
-        if not is_digit and new_index > 0 and new_index < LEN_ALPHA:
+        if not is_digit and 0 < new_index < LEN_ALPHA:
             new_letter = ALPHA_UPPER[new_index]
             return new_letter if letter.isupper() else new_letter.lower()
 
-        elif is_digit and new_index > 0 and new_index < LEN_DIGITS:
+        elif is_digit and 0 < new_index < LEN_DIGITS:
             return DIGITS[new_index]
 
         elif is_digit:
@@ -36,7 +33,6 @@ class CaesarCipher:
 
         new_letter = ALPHA_UPPER[new_index % LEN_ALPHA]
         return new_letter if letter.isupper() else new_letter.lower()
-
 
     def code_encoder_decoder(self, text: str, rot_type: int, status: str) -> dict:
 
