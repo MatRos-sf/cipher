50fb8e55ea678a7646c6b0430ffab99df459e7df
MatRos-sf <mateusz-rosenkranz@wp.pl>
2023-09-13 14:34:20 +0200

    feat(Buffer): add prottecton, typing and docstring

--- src/buffer/buffer.py
+++ src/buffer/buffer.py
@@ -3,12 +3,14 @@ from .text import Text
 
 from textwrap import shorten
 
+
 class Buffer:
     def __init__(self):
-        self.data = []
+        self.data: List[Text] = []
 
     def add(self, text: Text) -> None:
-        self.data.append(text)
+        if isinstance(text, Text):
+            self.data.append(text)
 
     def add_list_of_dict(self, arr: List[Dict[str, str]]) -> None:
         for text in arr:
@@ -19,8 +21,12 @@ class Buffer:
         return [text.__dict__ for text in self.data]
 
     def print_buffer(self) -> None:
-        print(f"{'idx':<4}| {'text':<100}| {'rot_type':<10}| {'status':<15}")
-        for idx, text in enumerate(self.data, start=1):
-            print(f"{idx:<4}  {shorten(text.text, width=100):<100}  {text.rot_type:<10}  {text.status:<15}")
-        print()
+        """ The function shows all data"""
+        if self.data:
+            print(f"{'idx':<4}| {'text':<100}| {'rot_type':<10}| {'status':<15}")
+            for idx, text in enumerate(self.data, start=1):
+                print(f"{idx:<4}  {shorten(text.text, width=100):<100}  {text.rot_type:<10}  {text.status:<15}")
+            print()
+        else:
+            print("Buffer is empty.\n")
 
